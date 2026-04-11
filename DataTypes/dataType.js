e5e424928fde380d278d26a35250af9ab93c77d6
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-11 09:55:48 +0530

    learned about datatypes in js

---
+++ DataTypes/dataType.js
@@ -0,0 +1,12 @@
+
+//           primitive dataType
+// number => 2
+// bigInt
+// string => ""
+// noolean => true/false
+// null => standalone value 
+// undefined => value not assigned yet
+// symbol => unique
+
+ let age = 18;
+ let name = "gulzar"
