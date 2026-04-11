6d204d97b1721e893813c5a7e78abadc8ce31dcc
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-11 12:09:15 +0530

    learned about operations

---
+++ Operations/Operations.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2="mehran"
+
+let str3 = str1+str2 
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log(1 +"2"+ 3);
+console.log("1" +2+ 3);  // first rule
+console.log(1+2+ "3");
+
+// if string first then  everthing becomes string
+// if number first => number
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2+2
+
+//prefix postfix 
+
+let x = 5;
+
+let y = ++x;
+
+console.log(x); // 6
+console.log(y); // 6
+
