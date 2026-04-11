fbcd03403308eb84c8d285cb78994c94d7a09e23
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-11 11:38:28 +0530

    Learned about data conversion

---
+++ DataTypes/conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(78);
+let numberCheck = "56abc"
+let valeNumber = Number(numberCheck)
+
+console.log(typeof valueInNumber);
+
+console.log(typeof valeNumber)
+console.log(valeNumber)
+
+let isHealthy = true;
+
+console.log(isHealthy)
+
+let isHealthyConvert = Number(isHealthy)
+
+console.log(isHealthyConvert)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn )
+
+
+// 1=> true  ; 0 => false
+// "" => false 
+// "hitesh" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
