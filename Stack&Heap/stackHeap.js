077477a7264c9217a86a58bedc21e95b81b8579d
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-12 20:39:36 +0530

    learned about heap and stack

---
+++ Stack&Heap/stackHeap.js
@@ -0,0 +1,8 @@
+
+// stack => Primitive
+// jo variable declarce hota hai usski copy banti hai 
+
+
+// Heap => Non-Primitive 
+// jo bhi varaible declare hota hai uskki refrence milti hai
+// jo bhi change hoga refrence mai woh orginal mai bhi
