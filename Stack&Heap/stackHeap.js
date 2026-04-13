14a971d603ce41a1aa7ef039f6b022ecace25db8
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-13 10:17:14 +0530

    Learned more about Stack and Heap

--- Stack&Heap/stackHeap.js
+++ Stack&Heap/stackHeap.js
@@ -6,3 +6,27 @@
 // Heap => Non-Primitive 
 // jo bhi varaible declare hota hai uskki refrence milti hai
 // jo bhi change hoga refrence mai woh orginal mai bhi
+
+let myName = "mehran"
+
+let anotherName = myName
+anotherName = "mehran mir"
+
+// myNAme ki copy di gayi thi , not myNAme direvtly , so change in copy does not change orginal data 
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "mehran789@gmail.com",
+    upi: "mehran@upi.com"
+}
+
+let userTwo = userOne
+
+userTwo.email = "mehran456@gmail.com"
+
+// value changed because of refrence , ismai copy nai di gayi thi
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
