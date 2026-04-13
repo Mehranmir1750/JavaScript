f9fc9aba16aa9efef4019b032a8de945f5e75c33
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-13 15:12:48 +0530

    Learned about random function , floor , around and more functions

---
+++ Nums_and_Maths/Maths.js
@@ -0,0 +1,20 @@
+console.log(Math);
+console.log(Math.abs(-5)); // negative to positive
+
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+
+console.log(Math.min(4,5,6,7,3,5,2,1));
+
+console.log(Math.random());  //values always between 0 and 1  
+
+
+const min = 1
+const max = 6
+
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+
+
