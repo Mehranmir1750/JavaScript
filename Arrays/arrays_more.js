5a6fdeacdea9f61840c1c01e4a118b0c0b6647f4
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-14 21:21:12 +0530

    Learned more about arrays

--- Arrays/arrays_more.js
+++ Arrays/arrays_more.js
@@ -21,3 +21,6 @@ const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 console.log(`real another array ${real_another_array}`);
 
+const last_array = [1,'zargar' , 4,7]
+console.log(`last array with different elements : ${last_array}`);
+
