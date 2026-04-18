47b1e9e798bd52cce202b7efe1f1d4f3637162a6
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-18 22:00:19 +0530

    day 1 of learing functions in js

---
+++ Functions/functions.js
@@ -0,0 +1,11 @@
+function sayMyName(){
+    console.log("M");
+    console.log("E");
+    console.log("H");
+    console.log("R");
+    console.log("A");
+    console.log("N");
+    
+}
+
+sayMyName()
