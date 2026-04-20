395abda9054cafa5dd7418b9d8bd429fea79fec7
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-20 20:45:46 +0530

    learned about true false values for condition in js

---
+++ Control_flow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = "m@gmail.com"
+
+if(userEmail){
+    console.log("Got user Email");
+}
+else{
+    console.log("Don't have user Email");
+    
+}
+
+// false values
+// false, 0 , -0 , BigInt -> 0n , "", null , undefined, NaN
+
+//truthy values
+// "0", "false", " ", [], {}, function(){}
