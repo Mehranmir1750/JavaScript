d98952b5832ce033f90e981f9765e029f9ab79b7
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-20 14:29:26 +0530

    learned about array function and different ways of returning values

--- Arrow_Function/arrow_function.js
+++ Arrow_Function/arrow_function.js
@@ -16,3 +16,48 @@ user.username = "sam"
 
 user.welcomeMessage()
 console.log(this);
+
+function chai(){
+    console.log(this);
+    
+}
+
+chai() 
+
+function chaiTwo(){
+    let username = "mehran"
+    console.log(this.username);   // can't use this inside function
+    // it gives undefined value
+    
+}
+
+chaiTwo() 
+
+//arrow function
+
+const chaiThree = () => {
+    let username = "mehran"
+    console.log(this.username);  //this cannot be used 
+}
+
+chaiThree()
+
+const addTwo = (num1,num2) => {
+    return num1+num2
+}
+
+console.log(addTwo(7,8));
+
+//implicit return 
+
+const addThree = (num1,num2, num3) =>  num1+num2+num3
+
+// if {} used then use return statement
+
+console.log(addThree(7,8, 5));
+
+const objectReturn = (num1,num2, num3) =>  ({username:"mehran"})
+
+console.log(objectReturn());
+
+
