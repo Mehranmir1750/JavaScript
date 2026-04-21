48b4c579a257a7e72ed1e0cf47bf520f7d700299
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-21 15:23:56 +0530

    learned about break and continue statements

--- Loops/for_loop.js
+++ Loops/for_loop.js
@@ -25,3 +25,38 @@ for (let j = 0; j < 10; j++) {
     
     
 }
+
+
+let myArray = ['ertugrul', 'osman', 'orhan']
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+    
+}
+
+//  break and continue 
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log('Detected 5 ');
+        break;
+    }
+    console.log(`Value of i is ${index}`);
+
+    
+}
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log('Detected 5 ');
+        continue;   // galti hogayi ek baari maaf kar dou
+        // condition skipped once but will not come out of loop
+    }
+    console.log(`Value of i is ${index}`);
+
+    
+    
+}
