df51f0121ad8f5f40385e2d0dcb8722344baa578
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-30 20:49:27 +0530

    learned another way of creating promise

--- Promises/promises.js
+++ Promises/promises.js
@@ -23,3 +23,14 @@ promiseOne.then(function(){
     console.log("Promise consumed");
     
 })
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        resolve()
+    }, 2000)
+}).then(function(){
+    console.log("Async 2 resolved");
+    
+})
