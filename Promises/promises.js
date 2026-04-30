9b7abfdb9650acfdae7052bc7834a9cafe5c6d37
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-30 20:34:39 +0530

    started to learn about promises

---
+++ Promises/promises.js
@@ -0,0 +1,25 @@
+/* 
+
+    Three stages of promises
+        pending: initial state, neither fulfilled nor rejected.
+        fulfilled: meaning that the operation was completed successfully.
+        rejected: meaning that the operation failed.
+    
+    */
+
+
+const promiseOne = new Promise(function(resolve,reject){
+    // do an async task 
+    // DB calls , cryptography , network call
+
+    setTimeout(function(){
+        console.log( 'Async task is completed');
+        resolve()
+        
+    }, 1000)
+}) 
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+    
+})
