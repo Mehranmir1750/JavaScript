99fdc673efb7737ead355b570cb7e35d9c731765
Mehranmir1750 <133797630+Mehranmir1750@users.noreply.github.com>
2026-04-30 23:05:58 +0530

    learining more about promises with try and catch block

--- Promises/promises.js
+++ Promises/promises.js
@@ -99,3 +99,24 @@ async function consumePromiseFive(){
     
 }
 consumePromiseFive()
+
+
+
+async function getAllUsers(){
+    
+    try {
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+        
+    } catch (error) {
+
+        console.log("E:", error);
+        
+        
+    }
+    
+}
+
+getAllUsers()
